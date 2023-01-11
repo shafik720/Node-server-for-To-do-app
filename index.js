e17dc9e2ab9bfb84113ab7ddfae8d3ce96671b8a
shafik720 <shafikrasel5@gmail.com>
2023-01-11 14:22:36 +0600

    adding to mongodb

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 
-app.post('/',(req, res)=>{
-    const result = req.body;
-    res.send(result);
-})
+
+
+const uri = "mongodb+srv://genius_car:AFpsnZTMmFrRqe3m@cluster0.0ihcm8w.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db('todo_app').collection('task_list');
+
+        app.post('/addTask', async(req, res)=>{
+            const doc = req.body;
+            const result = await userCollection.insertOne(doc);
+            res.send(result);
+        })
+        
+    }
+    finally { 
+
+    }
+}
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res)=>{
-    const user = {
-        name : 'russell'
-    }
+    const user = req.body;
     res.send(user);
 })
 
