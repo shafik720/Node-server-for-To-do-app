80772a159c9aba9d8ef4053dec07e09577a6cbc9
shafik720 <shafikrasel5@gmail.com>
2023-01-11 21:31:17 +0600

    working on delete single done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config();
 
@@ -31,6 +31,13 @@ async function run() {
             const query = {};
             const cursor = userCollection.find(query);
             const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.delete('/alltasks/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id :ObjectId(id)}
+            const result = await userCollection.deleteOne(query);
             res.send(result);
         })
     }
