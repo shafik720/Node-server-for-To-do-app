efba298a20fdcff959406cfaeb8a4e4fc1e0a7ad
shafik720 <shafikrasel5@gmail.com>
2023-01-13 18:44:22 +0600

    editing task on mongodb

--- index.js
+++ index.js
@@ -69,6 +69,13 @@ async function run() {
             const result = await userCollection.updateOne(filter, updatedDoc, options);
             res.send(result);
         })
+
+        app.get('/alltasks/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id : ObjectId(id)};
+            const result = await userCollection.findOne(query);
+            res.send(result);
+        })
     }
     finally { 
 
